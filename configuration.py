c426dac9d35eb7c04d44ee9b3d84d998b8aa0937
ge46tik <aditya.srinivas@tum.de>
2021-04-22 12:40:14 +0200

    Initial commit

---
+++ configuration.py
@@ -0,0 +1,20 @@
+import torch
+
+class training_config():
+    batch_size = 2
+    train_number_epochs = 
+    learning_rate = 0.0001
+    number_workers = 2
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    plot_frequency = 1
+
+class train_data_config():
+    training_set_dir = ""
+    training_ground_truth_dir = ""
+    training_data_size = 
+
+class validation_data_config():
+    validation_set_dir = ""
+    validation_ground_truth_dir = ""
+    validation_data_size = 
+
