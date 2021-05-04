e3299a152e86414c87594eed62c1b79bdd91bc7d
ge46tik <aditya.srinivas@tum.de>
2021-05-04 12:48:38 +0200

    Implemented FPN architecture for RPN

--- net.py
+++ net.py
@@ -1,5 +1,6 @@
 import torch.nn as nn
 import torch
+import torch.nn.functional as F
 
 #########################################################################################################
 ##                                       VGGNet Implementation                                         ## 
@@ -32,13 +33,13 @@ class VGG_Net(nn.Module):
         x = create_conv_layers(VGG16)
         return x
 
-############################################################################################################
-##                                        ResNet Implementation                                           ##  
-############################################################################################################
+#######################################################################################################################
+##                                              FPN Implementation                                                   ##           
+#######################################################################################################################
 class BasicBlock(nn.Module):
+    expansion = 4
     def  __init__(self,in_channels,out_channels,identity_downsample=None,stride=1):
         super(BasicBlock,self).__init__()
-        self.expansion = 4
         self.conv1 = nn.Conv2d(in_channels,out_channels,kernel_size=1,stride=stride,padding=1)
         self.bn1 = nn.BatchNorm2d(out_channels)
         self.conv2 = nn.Conv2d(out_channels,out_channels,kernel_size=3,stride=stride,padding=1)
@@ -69,56 +70,78 @@ class BasicBlock(nn.Module):
 
         return x
 
-class ResNet(nn.Module):
-    def __init__(self,block,layers,image_channels): #layers: Tells us how many times we reuse the resnet blocks. [3,4,6,3] for ResNet50
-        super(ResNet,self).__init__()
-        self.in_channels = 64
-        self.conv1 = nn.Conv2d(image_channels,64,kernel_size=7,stride=2,padding=3)
+class FPNRes(nn.Module):
+    def __init__(self,BasicBlock,num_blocks):
+        super(FPNRes,self).__init__()
+        self.in_planes = 64
+
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=7,stride=2,padding=3)
         self.bn1 = nn.BatchNorm2d(64)
-        self.relu = nn.ReLU()
-        self.maxpool = nn.MaxPool2d(kernel_size=3,stride=2,padding=1)
 
-        #ResNet layers
-        self.layer1 = self._make_layer(block,layers[0],out_channels=64,stride=1)
-        self.layer2 = self._make_layer(block,layers[1],out_channels=128,stride=2)
-        self.layer3 = self._make_layer(block,layers[2],out_channels=256,stride=2)
-        self.layer4 = self._make_layer(block,layers[3],out_channels=512,stride=2)
+        #Bottom-up layers
+        self.layer1 = self._make_layer(BasicBlock,  64, num_blocks[0], stride=1)
+        self.layer2 = self._make_layer(BasicBlock,  128, num_blocks[1], stride=2)
+        self.layer3 = self._make_layer(BasicBlock,  256, num_blocks[2], stride=2)
+        self.layer4 = self._make_layer(BasicBlock,  512, num_blocks[3], stride=2)
 
-        self.avgpool = nn.AdaptiveAvgPool2d((1,1))
+        #Top layer
+        self.toplayer = nn.Conv2d(2048, 256, kernel_size=1,stride=1,padding=0)
 
-    def forward(self,x):
-        x = self.conv1(x)
-        x = self.bn1(x)
-        x = self.relu(x)
-        x = self.maxpool(x)
+        #Smooth layers
+        self.smooth1 = nn.Conv2d(256, 256, kernel_size=3,stride=1,padding=1)
+        self.smooth2 = nn.Conv2d(256, 256, kernel_size=3,stride=1,padding=1)
+        self.smooth3 = nn.Conv2d(256, 256, kernel_size=3,stride=1,padding=1)
 
-        x = self.layer1(x)
-        x = self.layer2(x)
-        x = self.layer3(x)
-        x = self.layer4(x)
+        #Lateral layers
+        self.latlayer1 = nn.Conv2d(1024, 256, kernel_size=1,stride=1,padding=0)
+        self.latlayer2 = nn.Conv2d(512, 256, kernel_size=1,stride=1,padding=0)
+        self.latlayer3 = nn.Conv2d(256, 256, kernel_size=1,stride=1,padding=0)
 
-        x = self.avgpool(x)
-
-        return x
-
-    def _make_layer(self,BasicBlock,num_residual_blocks,out_channels,stride):
-        identity_downsample = None
+    def _make_layer(self,BasicBlock,planes,num_blocks,stride):
+        strides = [stride] + [1] * (num_blocks - 1)
         layers = []
-
-        if stride != 1 or self.in_channels != out_channels * 4:
-            identity_downsample = nn.Sequential(nn.Conv2d(self.in_channels,out_channels * 4,kernel_size=1,stride=stride),nn.BatchNorm2d(out_channels * 4))
+        for stride in strides:
+            layers.append(BasicBlock(self.in_planes,planes,stride))
+            self.in_planes = planes * BasicBlock.expansion
         
-        layers.append(BasicBlock(self.in_channels,out_channels,identity_downsample,stride))
-        self.in_channels = out_channels * 4 #256
-
-        for i in range(num_residual_blocks - 1):
-            layers.append(BasicBlock(self.in_channels,out_channels))
-
         return nn.Sequential(*layers)
 
-#######################################################################################################################
-##                                              FPN Implementation                                                   ##           
-#######################################################################################################################
+    def _upsample_add(self,x,y):
+        """
+        Upsample and add two feature maps.
+
+        Args:
+        x:Top feature map to be sampled
+        y:Lateral feature map
+
+        Returns:
+        Added feature map
+        """
+
+        _,_,H,W = y.size()
+        return F.upsample(x,size=(H,W),mode='bilinear') + y
+
+    def forward(self,x):
+        #Bottom-up
+        c1 = F.relu(self.bn1(self.conv1(x)))
+        c1 = F.max_pool2d(c1,kernel_size=3,stride=2,padding=1)
+        c2 = self.layer1(c1)
+        c3 = self.layer2(c2)
+        c4 = self.layer3(c3)
+        c5 = self.layer4(c4)
+
+        #Top-down
+        p5 = self.toplayer(c5)
+        p4 = self._upsample_add(p5, self.latlayer1(c4))
+        p3 = self._upsample_add(p4, self.latlayer2(c3))
+        p2 = self._upsample_add(p3, self.latlayer3(c2))
+
+        #Smooth
+        p4 = self.smooth1(p4)
+        p3 = self.smooth2(p3)
+        p2 = self.smooth3(p2)
+
+        return p2,p3,p4,p5
 
 
 
@@ -137,21 +160,4 @@ class ResNet(nn.Module):
 
 
 
-
-def ResNet18(img_channels=3):
-    return ResNet(BasicBlock,[2,2,2,2],img_channels)
-
-
-def ResNet50(img_channels=3):
-    return ResNet(BasicBlock,[3,4,6,3],img_channels)
-
-def ResNet101(img_channels=3):
-    return ResNet(BasicBlock,[3,4,23,3],img_channels)
-
-
-def test():
-    net = ResNet50()
-    x = torch.randn(2,3,600,600)
-    y = net(x).to('cuda')
-    #print(y.shape)
 
