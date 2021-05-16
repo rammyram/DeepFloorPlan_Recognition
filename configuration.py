67a1a8c618691131680b88a20e8fb6e3fd524fee
ge46tik <aditya.srinivas@tum.de>
2021-05-16 12:05:10 +0200

    Changed the variable names

--- configuration.py
+++ configuration.py
@@ -1,6 +1,6 @@
 import torch
 
-class training_config():
+def training_config():
     batch_size = 2
     train_number_epochs = 200
     learning_rate = 0.0001
@@ -8,15 +8,15 @@ class training_config():
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     plot_frequency = 1
 
-class train_data_config():
+def train_data_config():
     training_set_dir = "/home/aditya/Documents/HiWi/Work/Deep_Floor_Plan_Recognition/Code/DeepFloorPlan_Recognition/R3D_doors and windows/Train/Images/Image_train/"
     doors_training_ground_truth_dir = "/home/aditya/Documents/HiWi/Work/Deep_Floor_Plan_Recognition/Code/DeepFloorPlan_Recognition/R3D_doors and windows/Train/doors/doors_train/"
     windows_training_ground_truth_dir = "/home/aditya/Documents/HiWi/Work/Deep_Floor_Plan_Recognition/Code/DeepFloorPlan_Recognition/R3D_doors and windows/Train/windows/windows_train/"
     training_data_size = 146
 
-class validation_data_config():
+def validation_data_config():
     validation_set_dir = "/home/aditya/Documents/HiWi/Work/Deep_Floor_Plan_Recognition/Code/DeepFloorPlan_Recognition/R3D_doors and windows/Train/Images/Imgaes_val/"
-    validation_ground_truth_dir = "/home/aditya/Documents/HiWi/Work/Deep_Floor_Plan_Recognition/Code/DeepFloorPlan_Recognition/R3D_doors and windows/Train/doors/doors_val/"
+    doors_validation_ground_truth_dir = "/home/aditya/Documents/HiWi/Work/Deep_Floor_Plan_Recognition/Code/DeepFloorPlan_Recognition/R3D_doors and windows/Train/doors/doors_val/"
     windows_val_groud_truth_dir = "/home/aditya/Documents/HiWi/Work/Deep_Floor_Plan_Recognition/Code/DeepFloorPlan_Recognition/R3D_doors and windows/Train/windows/windows_val/"
     validation_data_size = 27
 
