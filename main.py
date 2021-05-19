1f6a917a182d3bd13cedf0f1192b527513d28c42
ge46tik <aditya.srinivas@tum.de>
2021-05-20 00:37:48 +0200

    Integrated the wandb initializer of train.py

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+from torch.utils.data import dataset
 import wandb
-import config
+import configuration
+import train
 
-params = 
+params = dict(epochs = configuration.training_config.number_epochs, batch_size = configuration.training_config.batch_size, lr = configuration.training_config.learning_rate, dataset_size = configuration.train_data_config.training_data_size)
 
 if __name__ == "__main__":
     wandb.login()
-    model = 
+    model = train.wandb_initializer(params)
