1d13abc7e35b0f8f3daa55f3d3118471a3a6a244
ge46tik <aditya.srinivas@tum.de>
2021-05-26 22:51:26 +0200

    changes to the data_transforms

--- train.py
+++ train.py
@@ -20,7 +20,7 @@ def wandb_initializer(args):
     return model
 
 def nn_model(config):
-    data_transformers = transforms.Compose([transforms.ToTensor()])
+    data_transformers = transforms.Compose([transforms.ToTensor(), transforms.Resize(600)])
 
     train_set = FloorPlanDataset(image_dir=configuration.train_data_config.training_set_dir,door_dir=configuration.train_data_config. doors_training_ground_truth_dir,window_dir=configuration.train_data_config.windows_training_ground_truth_dir)
     val_set = FloorPlanDataset(image_dir=configuration.validation_data_config.validation_set_dir,door_dir=configuration.validation_data_config. doors_validation_ground_truth_dir,window_dir=configuration.validation_data_config.windows_validation_ground_truth_dir)
