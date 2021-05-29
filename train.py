d777a17e568943af0a313775ea81542e2e17a322
ge46tik <aditya.srinivas@tum.de>
2021-05-29 11:25:00 +0200

    Debugging

--- train.py
+++ train.py
@@ -82,9 +82,9 @@ def train(nn_model,train_set_loader,val_set_loader,loss_func,optimizer,config):
             else:
                 image,door = image, door
             
-            output = nn_model(image)
-            print(type(output))
-            loss_door = loss_func(output, door)
+            output = nn_model(image.unsqueeze(0))
+            
+            loss_door = loss_func(output, door.unsqueeze(0))
             #loss_window = loss_func(output[1].float(),window.float())
             
             
