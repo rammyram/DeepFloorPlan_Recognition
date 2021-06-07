fd820ff7fb8a30ed4a9b537f21d40eda7d82e035
ge46tik <aditya.srinivas@tum.de>
2021-06-07 10:38:47 +0200

    Added gt details

--- dataloader.py
+++ dataloader.py
@@ -5,10 +5,9 @@ import os
 import torch
 
 class FloorPlanDataset(Dataset):
-    def __init__(self,image_dir,door_dir,transform=None):
+    def __init__(self,image_dir,gt_dir,transform=None):
         self.image_dir = image_dir
-        self.door_dir = door_dir
-        #self.window_dir = window_dir
+        self.gt_dir = gt_dir
         self.transform = transform
         self.images = os.listdir(image_dir)
 
@@ -17,20 +16,15 @@ class FloorPlanDataset(Dataset):
 
     def __getitem__(self, index):
         image_path = os.path.join(self.image_dir,self.images[index])
-        door_path = os.path.join(self.door_dir,self.images[index].replace('.jpg','_doors.png'))
-        #window_path = os.path.join(self.window_dir,self.images[index].replace('.jpg','_windows.png'))
+        gt_path = os.path.join(self.gt_dir,self.images[index])
 
         image = Image.open(image_path).convert('L')
         image = image.resize((600,600),Image.ANTIALIAS)
         image = np.array(image,dtype=np.float32)
         
-        doors = Image.open(door_path).convert('L')
-        doors = doors.resize((600,600),Image.ANTIALIAS)
-        doors = np.array(doors,dtype=np.float32)
+        gt = Image.open(gt_path)
+        gt = np.array(gt,dtype=np.float32)
         
-        #windows = Image.open(window_path).convert('L')
-        #windows = windows.resize((600,600),Image.ANTIALIAS)
-        #windows = np.array(windows,dtype=np.float32)
         
 
         #doors[doors == 255.0] = 1.0
@@ -38,7 +32,7 @@ class FloorPlanDataset(Dataset):
         
         if self.transform is not None:
             image = torch.tensor([image])
-            doors = torch.tensor([doors])
-            #windows = torch.tensor([windows])
+            gt = torch.tensor([gt])
+            
         
-        return image, doors
+        return image, gt
