13fc796435cc7773c66477dd33e0d34edbf24867
ge46tik <aditya.srinivas@tum.de>
2021-06-07 10:31:35 +0200

    Bug fix

--- configuration.py
+++ configuration.py
@@ -10,13 +10,13 @@ class training_config():
 
 class train_data_config():
     training_set_dir = "/app/DeepFloorplan_Recognition/Data/Images_train/"
-    ground_truth_dir = "/app/DeepFloorplan_Recognition/Data/gt_train/"
+    train_ground_truth_dir = "/app/DeepFloorplan_Recognition/Data/gt_train/"
     #windows_training_ground_truth_dir = "/app/DeepFloorplan_Recognition/Data/windows_train/"
     training_data_size = 10
 
 class validation_data_config():
     validation_set_dir = "/app/DeepFloorplan_Recognition/Data/Images_val/"
-    doors_validation_ground_truth_dir = "/app/DeepFloorplan_Recognition/Data/gt_val/"
+    validation_ground_truth_dir = "/app/DeepFloorplan_Recognition/Data/gt_val/"
     #windows_validation_ground_truth_dir = "/app/DeepFloorplan_Recognition/Data/windows_val/"
     validation_data_size = 5
 
