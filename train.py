88b3e0ad58f2dea6db2135f62c30dd6021f77619
ge46tik <aditya.srinivas@tum.de>
2021-06-16 15:15:09 +0200

    Bug fix

--- train.py
+++ train.py
@@ -83,7 +83,7 @@ def train(nn_model,train_set_loader,val_set_loader,loss_func,optimizer,config):
             image = image.reshape([image.shape[0],image.shape[-1],image.shape[2],image.shape[1]])
             gt = gt.squeeze(1)
             gt = gt.reshape([gt.shape[0],gt.shape[3],gt.shape[2],gt.shape[1]])
-            
+            print(gt.shape)
             nn_model.train()
             if(configuration.training_config.device.type == 'cuda'):
                 image,gt = image.cuda(), gt.cuda()
