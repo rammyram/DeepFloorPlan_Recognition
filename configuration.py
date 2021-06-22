50d13a06d385fbf4249bd543aa01ce1c5758b929
ge46tik <aditya.srinivas@tum.de>
2021-06-22 11:50:31 +0200

    changed the config params

--- configuration.py
+++ configuration.py
@@ -2,19 +2,19 @@ import torch
 
 class training_config():
     batch_size = 4
-    number_epochs = 60
+    number_epochs = 20
     learning_rate = 0.0001
     number_workers = 2
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    plot_frequency = 1
+    plot_frequency = 5
 
 class train_data_config():
     training_set_dir = "/content/DeepFloorPlan_Recognition/Data/Images_train/"
     train_ground_truth_dir = "/content/DeepFloorPlan_Recognition/Data/ground_truth_train/"
-    training_data_size = 10
+    training_data_size = 70
 
 class validation_data_config():
     validation_set_dir = "/content/DeepFloorPlan_Recognition/Data/Images_val/"
     validation_ground_truth_dir = "/content/DeepFloorPlan_Recognition/Data/ground_truth_val/"
-    validation_data_size = 5
+    validation_data_size = 10
 
