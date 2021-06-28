80e1bac1d1f8fd0d6aabb668c4979b87888a6f51
ge46tik <aditya.srinivas@tum.de>
2021-06-29 00:30:41 +0200

    bug fix

--- train.py
+++ train.py
@@ -57,6 +57,7 @@ def validation(nn_model,val_set_loader,loss_function):
     for batch_id,(image,gt) in enumerate(val_set_loader):
         image = image.squeeze(1)
         image = image.reshape([image.shape[0],image.shape[-1],image.shape[2],image.shape[1]])
+        print(np.shape(gt))
         gt = gt.squeeze(1)
         gt = gt.reshape([gt.shape[0],gt.shape[3],gt.shape[2],gt.shape[1]])
         if(configuration.training_config.device.type == 'cuda'):
