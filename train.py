352ca162941e6b0608194554942d73715c4dec75
ge46tik <aditya.srinivas@tum.de>
2021-06-29 00:46:26 +0200

    debugging

--- train.py
+++ train.py
@@ -102,6 +102,7 @@ def train(nn_model,train_set_loader,val_set_loader,loss_func,optimizer, config):
             else:
                 image,gt = image, gt
             
+            print(type(gt),np.shape(gt))
             output = nn_model(image)
             loss = loss_func(output, gt)
             
