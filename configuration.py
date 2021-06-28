44cd140e22d8f32968a10a46ee9ba35576a9d08a
ge46tik <aditya.srinivas@tum.de>
2021-06-28 09:57:32 +0200

    Changed file path

--- configuration.py
+++ configuration.py
@@ -3,18 +3,18 @@ import torch
 class training_config():
     batch_size = 2
     number_epochs = 10
-    learning_rate = 0.00001
+    learning_rate = 0.000001
     number_workers = 2
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     plot_frequency = 5
 
 class train_data_config():
-    training_set_dir = "/app/DeepFloorplan_Recognition/Data/Images_train/"
-    train_ground_truth_dir = "/app/DeepFloorplan_Recognition/Data/ground_truth_train/"
+    training_set_dir = "/content/DeepFloorplan_Recognition/Data/Images_train/"
+    train_ground_truth_dir = "/content/DeepFloorplan_Recognition/Data/ground_truth_train/"
     training_data_size = 40
 
 class validation_data_config():
-    validation_set_dir = "/app/DeepFloorplan_Recognition/Data/Images_val/"
-    validation_ground_truth_dir = "/app/DeepFloorplan_Recognition/Data/ground_truth_val/"
+    validation_set_dir = "/content/DeepFloorplan_Recognition/Data/Images_val/"
+    validation_ground_truth_dir = "/content/DeepFloorplan_Recognition/Data/ground_truth_val/"
     validation_data_size = 10
 
