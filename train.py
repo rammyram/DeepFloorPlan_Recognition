fe5b76be46b1a738c5e77752a62635bb8b8d7611
ge46tik <aditya.srinivas@tum.de>
2021-06-29 20:35:16 +0200

    debug

--- train.py
+++ train.py
@@ -105,7 +105,7 @@ def train(nn_model,train_set_loader,val_set_loader,loss_func,optimizer, config):
             
             
             output = nn_model(image)
-            loss = loss_func(output, gt.unsqueeze(1))
+            loss = loss_func(output, gt)
             
             
             optimizer.zero_grad()
