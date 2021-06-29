75aa6f1b674637352e92be799f8b3265fe09505b
ge46tik <aditya.srinivas@tum.de>
2021-06-29 20:34:11 +0200

    debug

--- dataloader.py
+++ dataloader.py
@@ -41,9 +41,10 @@ class FloorPlanDataset(Dataset):
 
         #plt.imsave(self.images[index],arr=gt/255)       
         if self.transform is not None:
-            image = torch.tensor([image])
+            image = torch.tensor([image],dtype=torch.float)
             gt = torch.tensor([gt],dtype=torch.long)
         
+        print(type(gt))
         #print(np.shape(image),np.shape(gt))
         
         return image, gt
