9660d51c62526c7a1de279f5263411f325289a92
ge46tik <aditya.srinivas@tum.de>
2021-06-29 19:50:14 +0200

    pytorch implementation of CE

--- loss.py
+++ loss.py
@@ -1,21 +1,22 @@
-import numpy as np
+import torch
+from torch import autograd
+from torch import nn
 
-def cross_entropy(X,y):
-    """
-    X is the output from fully connected layer (num_examples x num_classes)
-    y is labels (num_examples x 1)
-    	Note that y is not one-hot encoded vector. 
-    	It can be computed as y.argmax(axis=1) from one-hot encoded vectors of labels if required.
-    """
-    m = y.shape[0]
-    p = softmax(X)
-    # We use multidimensional array indexing to extract 
-    # softmax probability of the correct label for each sample.
-    # Refer to https://docs.scipy.org/doc/numpy/user/basics.indexing.html#indexing-multi-dimensional-arrays for understanding multidimensional array indexing.
-    log_likelihood = -np.log(p[range(m),y])
-    loss = np.sum(log_likelihood) / m
-    return loss
 
-def softmax(X):
-    exps = np.exp(X)
-    return exps / np.sum(exps)
+class CrossEntropyLoss(nn.Module):
+    """
+    This criterion (`CrossEntropyLoss`) combines `LogSoftMax` and `NLLLoss` in one single class.
+    
+    NOTE: Computes per-element losses for a mini-batch (instead of the average loss over the entire mini-batch).
+    """
+    log_softmax = nn.LogSoftmax()
+
+    def __init__(self, class_weights):
+        super().__init__()
+        self.class_weights = autograd.Variable(torch.FloatTensor(class_weights).cuda())
+
+    def forward(self, logits, target):
+        log_probabilities = self.log_softmax(logits)
+        # NLLLoss(x, class) = -weights[class] * x[class]
+        return -self.class_weights.index_select(0, target) * log_probabilities.index_select(-1, target).diag()
+    
