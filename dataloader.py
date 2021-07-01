4f357e41755f753860d3446364b191466e6d00f4
ge46tik <aditya.srinivas@tum.de>
2021-07-01 10:54:14 +0200

    debug

--- dataloader.py
+++ dataloader.py
@@ -37,7 +37,7 @@ class FloorPlanDataset(Dataset):
         gt[np.all(gt == 255)] = 1
 
         image = image.reshape([image.shape[-1],image.shape[0],image.shape[1]])
-        #gt = gt.reshape([gt.shape[-1],gt.shape[0],gt.shape[1]])
+        gt = gt.reshape([gt.shape[-1],1,gt.shape[0],gt.shape[1]])
         
 
         #plt.imsave(self.images[index],arr=gt/255)       
