12883075c59905e6cf894dde944d606e22693fdc
ge46tik <aditya.srinivas@tum.de>
2021-07-05 08:41:03 +0200

    added label_img_to_rgb

--- dataloader.py
+++ dataloader.py
@@ -6,6 +6,21 @@ from torch.utils.data import Dataset
 import os
 import torch
 
+SEG_LABELS_LIST = [
+    {"id":-1,"name":"void","rgb_values":[0,0,0]},
+    {"id":0,"name":"wall","rgb_values":[128,0,0]}
+]
+
+def label_img_to_rgb(label_img):
+    label_img =  np.squeeze(label_img)
+    labels = np.unique(label_img)
+    label_infos = [l for l in SEG_LABELS_LIST if l['id'] in labels]
+    label_img_rgb = np.array([label_img,label_img,label_img]).transpose(1,2,0)
+    for l in label_infos:
+        mask = label_img == l['id']
+        label_img_rgb[mask] = l['rgb_values']
+    
+    return label_img_rgb.astype(np.uint8)
 
 class FloorPlanDataset(Dataset):
     def __init__(self,image_dir,gt_dir,transform=False):
