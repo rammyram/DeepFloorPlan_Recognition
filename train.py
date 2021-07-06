17ecbee56c28d72cc9e8d61330621afacf3379b8
ge46tik <aditya.srinivas@tum.de>
2021-07-06 10:26:32 +0200

    added file extension to save output

--- train.py
+++ train.py
@@ -155,4 +155,4 @@ def visualizer(pred_image,epoch,config):
             pred_imgs = class_labels_list[i]
     
         for p in pred_imgs:
-            plt.imsave("image",p)
+            plt.imsave("image.png",p)
