f8ae03b25db5cfb26b00d48afab43a8a5417f04f
ge46tik <aditya.srinivas@tum.de>
2021-07-06 22:36:11 +0200

    printing dimension of gt

--- train.py
+++ train.py
@@ -100,6 +100,7 @@ def train(nn_model,train_set_loader,val_set_loader,loss_func,optimizer, config):
         #print("\nLearning rate at this epoch is: %0.9f"%scheduler.get_lr()[0])
         for batch_id,(image,gt) in enumerate(train_set_loader):
             image = image.squeeze(1)
+            print(gt.shape)
             gt = gt.squeeze(1)
             
             nn_model.train()
