73c6c0fab9faf6aa476586e3843bc67eeef0ea15
ge46tik <aditya.srinivas@tum.de>
2021-07-06 22:37:20 +0200

    squeeze dimension 1 of gt

--- dataloader.py
+++ dataloader.py
@@ -69,4 +69,4 @@ class FloorPlanDataset(Dataset):
         #gt = gt.type(torch.LongTensor)
         #print(np.shape(image),np.shape(gt))
         
-        return image, gt
+        return image, gt.squeeze(1)
