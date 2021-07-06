4756ecd6faf795a37c6edb8af0052f8fca3a0b5d
ge46tik <aditya.srinivas@tum.de>
2021-07-06 22:52:44 +0200

    print predicted output

--- train.py
+++ train.py
@@ -111,6 +111,7 @@ def train(nn_model,train_set_loader,val_set_loader,loss_func,optimizer, config):
                 image,gt = image, gt
             
             output = nn_model(image)
+            print(output)
             loss = loss_func(output, gt)    
             
             optimizer.zero_grad()
