c88016d6b9c0437428be55f4d43b3e5645bc2c46
ge46tik <aditya.srinivas@tum.de>
2021-07-06 10:08:43 +0200

    reshaping gt

--- dataloader.py
+++ dataloader.py
@@ -44,8 +44,8 @@ class FloorPlanDataset(Dataset):
         image = np.array(image,dtype=np.float32).transpose(2,0,1)
         
         gt = Image.open(gt_path).convert("L")
-        gt = np.array(gt,dtype=np.float32)
-        print(np.shape(gt))
+        gt = np.array(gt,dtype=np.float32).transpose(2,0,1)
+        
         """
         target_labels = gt[...,0]
         for label in SEG_LABELS_LIST:
