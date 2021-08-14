4a9d91e66e5351469de5bdaabbdef264d1962943
ge46tik <aditya.srinivas@tum.de>
2021-08-14 14:41:37 +0200

    bug fix

--- train.py
+++ train.py
@@ -96,7 +96,7 @@ def train(nn_model,train_set_loader,val_set_loader,loss_func,optimizer, config):
                 image,gt = image.cuda(),gt.cuda()
             else:
                 image,gt = image, gt
-            print(image.shape)
+        
             output = nn_model(image)
             loss = loss_func(output, gt)    
             
