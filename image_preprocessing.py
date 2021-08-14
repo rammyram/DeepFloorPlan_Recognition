0fca19c79122715cdf115a8e77436383e20db08a
ge46tik <aditya.srinivas@tum.de>
2021-08-14 13:23:30 +0200

    Changed the way images are to be emrged to create ground truth data

--- image_preprocessing.py
+++ image_preprocessing.py
@@ -1,8 +1,9 @@
 import cv2
 import numpy as np
-from PIL import Image
+from PIL import Image,ImageOps
 import matplotlib.pyplot as plt
 import os
+from tqdm import tqdm
 
 def resize_images(source_dir1):
     path, dirs, files = next(os.walk(source_dir1))
@@ -36,6 +37,7 @@ def merge_images(source_dir1, source_dir2):
         file_1.append(int(files[i][0]))
     file_1 = sorted(file_1)
     
+    
     path, dirs, files = next(os.walk(source_dir2))
     file_2 = []
     for i in range(len(files)):
@@ -44,17 +46,23 @@ def merge_images(source_dir1, source_dir2):
     file_2 = sorted(file_2)
 
     
-    for i in range(len(file_1)):
-        src_img = cv2.imread(source_dir1 + str(file_1[i]) + "_doors.png")
+    for i in tqdm(range(len(file_1))):
+        src_img = plt.imread(source_dir1 + str(file_1[i]) + "_wall.png")
+        src_img = cv2.resize(src_img,(600,600))
         src_img = cv2.cvtColor(src_img,cv2.COLOR_BGR2RGB)
-        dest_img = cv2.imread(source_dir2 + str(file_2[i]) + "_windows.png")
-        dest_img = cv2.cvtColor(dest_img,cv2.COLOR_BGR2RGB)
+        src_img[src_img == (1.0,1.0,1.0)] = 255
+        src_img[:,:,1] = src_img[:,:,2] = 0
+        
+        dest_img = plt.imread(source_dir2 + str(file_2[i]) + "_doors.png")
+        dest_img = cv2.resize(dest_img,(600,600))
+        dest_img = cv2.cvtColor(dest_img, cv2.COLOR_BGR2RGB)
+        dest_img[dest_img == 1.0] = 255
+        dest_img[:,:,0] = dest_img[:,:,2] = 0
 
-        src_img = Image.fromarray(src_img)
-        dest_img = Image.fromarray(dest_img)
-
-        new_img = Image.blend(src_img,dest_img,0.5)
-        new_img.save("/home/aditya/Documents/HiWi/Work/R3D_doors and windows/new_val/" + str(file_1[i]) + ".png")
+        
+        new_img = cv2.addWeighted(src_img, 1.0, dest_img, 1.0, 0.0)
+        new_img = cv2.cvtColor(new_img,cv2.COLOR_BGR2RGB)
+        cv2.imwrite("/home/aditya/Documents/HiWi/Work/R3D_doors and windows/new_train/" + str(file_1[i]) + ".png",new_img)
   
 def dimension_check(source_file):
     path, dirs, files = next(os.walk(source_file))
@@ -67,12 +75,36 @@ def dimension_check(source_file):
     print("Number of images checked: ",len(files))
 
 
+def create_ground_truth_images(source_dir,destination_dir):
+    path, dirs, files = next(os.walk(source_dir))
 
-src_dir1 = "/home/aditya/Documents/HiWi/Work/R3D_doors and windows/Train/Images/Images_val/"
-src_dir2 = "/home/aditya/Documents/HiWi/Work/R3D_doors and windows/Train/windows/windows_val_new/"
-#destination_dir = "/home/aditya/Documents/HiWi/Work/R3D_doors and windows/Train/doors/doors_val_color/"
-#merge_images(src_dir1,src_dir2)
-dimension_check(src_dir1)
+    for i in range(len(files)):
+        if(files[i].endswith(".png")):
+            image = Image.open(source_dir + str(files[i]))
+            image = ImageOps.invert(image)
+            image.save(destination_dir + str(files[i]))
+    print("Completed creating initial version of ground truth images")
+
+
+def seperate_wall_images(source_dir,destination_dir):
+    path, dirs, files = next(os.walk(source_dir))
+
+    for i in range(len(files)):
+        if(files[i].endswith('_close_wall.png')):
+            image = Image.open(source_dir + str(files[i]))
+            image = image.resize((600,600),Image.ANTIALIAS)
+            image.save(destination_dir + str(files[i]))
+    
+    print("Completed traferring wall images into a seperate folder")
+
+
+
+src_dir2 = "/home/aditya/Documents/HiWi/Work/R3D_doors and windows/Train/doors/doors_train/"
+src_dir1 = "/home/aditya/Documents/HiWi/Work/R3D_doors and windows/Train/Walls/Wall_train/"
+#destination_dir = "/home/aditya/Documents/HiWi/Work/R3D_doors and windows/Train/Walls/Close_wall/"
+merge_images(src_dir1,src_dir2)
+#dimension_check(src_dir1)
 #resize_images(src_dir2)
 #change_mask_color(src_dir1,destination_dir)
+#seperate_wall_images(src_dir1,destination_dir)
 
