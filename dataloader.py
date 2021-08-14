17bff2291dff0ab8833488a72498db612bf9ae7b
ge46tik <aditya.srinivas@tum.de>
2021-08-14 10:35:24 +0200

    bug fix while returning gt_labels

--- dataloader.py
+++ dataloader.py
@@ -47,5 +47,5 @@ class FloorPlanDataset(Dataset):
 
         #gt_labels = np.transpose(gt_labels,(2,1,0))        
         gt_labels = torch.from_numpy(gt_labels.copy())
-        gt_labels = torch.to(dtype=toch.long)
+        gt_labels = gt_labels.to(dtype=toch.long)
         return floor_plan, gt_labels
