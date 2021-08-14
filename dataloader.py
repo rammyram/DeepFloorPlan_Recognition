4e20dd5f361fd6a54942c905f721e22b82dfe34c
ge46tik <aditya.srinivas@tum.de>
2021-08-14 17:44:11 +0200

    bug fix

--- dataloader.py
+++ dataloader.py
@@ -32,10 +32,10 @@ class FloorPlanDataset(Dataset):
         floor_plan = plt.imread(image_path)
 
         floor_plan_resized = cv2.resize(floor_plan,(256,256))
-        print(np.shape(floor_plan_resized))
+        #print(np.shape(floor_plan_resized))
         floor_plan_resized = floor_plan / floor_plan.max()
         floor_plan_resized = np.transpose(floor_plan_resized,[2,1,0]).astype(np.float32)
-        #print(np.shape(floor_plan_resized))
+        print(np.shape(floor_plan_resized))
         floor_plan = torch.from_numpy(floor_plan_resized)
         
 
