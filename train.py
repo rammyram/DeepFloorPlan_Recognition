f04dcc1c867c7c0b2dfa3d9cd85f668e2b8961ae
ge46tik <aditya.srinivas@tum.de>
2021-08-14 14:28:38 +0200

    Removed transform arg in dataloader

--- train.py
+++ train.py
@@ -30,8 +30,8 @@ def wandb_initializer(args):
 def nn_model(config):
     #data_transformers = transforms.Compose([transforms.ToTensor()])
 
-    train_set = FloorPlanDataset(image_dir=configuration.train_data_config.training_set_dir,gt_dir=configuration.train_data_config.train_ground_truth_dir,transform=True)
-    val_set = FloorPlanDataset(image_dir=configuration.validation_data_config.validation_set_dir,gt_dir=configuration.validation_data_config.validation_ground_truth_dir,transform=True)
+    train_set = FloorPlanDataset(image_dir=configuration.train_data_config.training_set_dir,gt_dir=configuration.train_data_config.train_ground_truth_dir)
+    val_set = FloorPlanDataset(image_dir=configuration.validation_data_config.validation_set_dir,gt_dir=configuration.validation_data_config.validation_ground_truth_dir)
 
     #Loading train and val set
     train_set_loader = DataLoader(train_set,batch_size = configuration.training_config.batch_size,shuffle=False,num_workers=configuration.training_config.number_workers)
