268ff1488b94a5249892961ccc80045960c04128
ge46tik <aditya.srinivas@tum.de>
2021-08-14 10:32:01 +0200

    returns gt_labels

--- dataloader.py
+++ dataloader.py
@@ -47,5 +47,5 @@ class FloorPlanDataset(Dataset):
 
         #gt_labels = np.transpose(gt_labels,(2,1,0))        
         gt_labels = torch.from_numpy(gt_labels.copy())
- 
-        return floor_plan, gt
+        
+        return floor_plan, gt_labels
