dd198f7d3d56bf33e83b4aa991a047392ac41d92
ge46tik <aditya.srinivas@tum.de>
2021-08-14 12:31:17 +0200

    Print maximum of ground truth labels

--- dataloader.py
+++ dataloader.py
@@ -48,4 +48,5 @@ class FloorPlanDataset(Dataset):
         #gt_labels = np.transpose(gt_labels,(2,1,0))        
         gt_labels = torch.from_numpy(gt_labels.copy())
         gt_labels = gt_labels.to(dtype=torch.long)
+        print("Maximum of ground truth:",gt_labels.max())
         return floor_plan, gt_labels
