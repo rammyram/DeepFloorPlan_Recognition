32b5f545848b6792f470e88b62bfe1ea2d680568
ge46tik <aditya.srinivas@tum.de>
2021-08-14 17:39:34 +0200

    resizing the floor plan image

--- dataloader.py
+++ dataloader.py
@@ -30,6 +30,7 @@ class FloorPlanDataset(Dataset):
         gt_path = gt_path.replace(".jpg",".png")
 
         floor_plan = plt.imread(image_path)
+        floor_plan_resized = cv2.resize(floor_plan,cv2.COLOR_BGR2RGB)
         floor_plan_resized = floor_plan / floor_plan.max()
         floor_plan_resized = np.transpose(floor_plan_resized,[2,1,0]).astype(np.float32)
         #print(np.shape(floor_plan_resized))
